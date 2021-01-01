e4d0cfd764c80acb0bd47c6eb909820c1776b4ae
MrGibus <arcane2.0@gmail.com>
2021-01-01 22:07:12 +0800

    Created Landing App

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,100 @@
+#![allow(non_snake_case)]
+
 mod counter;
 mod temperature;
 mod flights;
 mod timer;
 mod crud;
 
-// fn main() -> iced::Result {
-//     // counter::main()
-//     // temperature::main()
-//     // flights::main()
-//     crud::main()
-// }
+use iced::{button, Settings, Button, Column, Sandbox, Element, Text, HorizontalAlignment, Length};
 
-use std::io;
+fn main() -> iced::Result {
+    let mut settings = Settings::default();
+    settings.window.size = (200, 400);
+    settings.window.resizable = false;
+    Landing::run(settings)
+}
 
-/// Entry point into program
-pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Enter example 1 - 7");
-    println!("1: Counter");
-    println!("2: Temperature Converter");
-    println!("3: Flight Booker");
-    println!("4: Timer");
-    println!("5: CRUD (create, read, update, delete)");
-    println!("6: Circle Drawing");
-    println!("7: Cells");
 
-    let mut input: String = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {
-            println!("{}", input);
-        }
-        Err(error) => println!("Error: {}", error)
-    };
+#[derive(Default)]
+struct Landing{
+    counter: button::State,
+    temperature: button::State,
+    flights: button::State,
+    timer: button::State,
+    crud: button::State,
+    circle: button::State,
+    cells: button::State,
+}
 
-    let v = input.trim().parse::<u32>().expect("please enter a number from 1- 7");
-    match v {
-        1 => {
-            println!("Counter Selected");
-            counter::main()?
-        },
-        2 => {
-            println!("Temperature Converter Selected");
-            temperature::main()?
-        },
-        3 => {
-            println!("Flight Booker Selected");
-            flights::main()?
-        },
-        4 => {
-            println!("Timer Selected");
-            timer::main()?
-        },
-        5 => {
-            println!("CRUD Selected");
-            crud::main()?
-        },
-        6 => {
-            println!("Circle drawer Selected");
-            println!("NOT YET IMPLEMENTED");
-        },
-        7 => {
-            println!("Cells Selected");
-            println!("NOT YET IMPLEMENTED");
-        },
-        _ => ()
+
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+enum Message{
+    Counter,
+    Temperature,
+    Flights,
+    Timer,
+    CRUD,
+    Circle,
+    Cells,
+}
+
+impl Sandbox for Landing {
+    type Message = Message;
+
+    fn new() -> Self {Self::default()}
+    fn title(&self) -> String {"7Guis - Iced".to_string()}
+
+    fn update(&mut self, message: Message) {
+         match message {
+            Message::Counter => counter::main(),
+            Message::Temperature => temperature::main(),
+            Message:: Flights => flights::main(),
+            Message::Timer => timer::main(),
+            Message::CRUD => crud::main(),
+            _ => Ok(())
+        }.unwrap_or(());
     }
-    Ok(())
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .padding(10)
+            .spacing(5)
+            .push(Text::new("7GUIs\nfor\nIced")
+                .width(Length::Fill)
+                .size(40)
+                .color([0.1, 0.1, 0.6])
+                .horizontal_alignment(HorizontalAlignment::Center)
+            )
+            .push(
+                Button::new(&mut self.counter, Text::new("Counter")
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                    .width(Length::Fill)).width(Length::Fill).on_press(Message::Counter))
+            .push(
+                Button::new(&mut self.temperature, Text::new("Temperature")
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                    .width(Length::Fill)).width(Length::Fill).on_press(Message::Temperature))
+            .push(
+                Button::new(&mut self.flights, Text::new("Flights")
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                    .width(Length::Fill)).width(Length::Fill).on_press(Message::Flights))
+            .push(
+                Button::new(&mut self.timer, Text::new("Timer")
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                    .width(Length::Fill)).width(Length::Fill).on_press(Message::Timer))
+            .push(
+                Button::new(&mut self.crud, Text::new("CRUD")
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                    .width(Length::Fill)).width(Length::Fill).on_press(Message::CRUD))
+            .push(
+                Button::new(&mut self.circle, Text::new("Circles")
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                    .width(Length::Fill)).width(Length::Fill))
+            .push(
+                Button::new(&mut self.cells, Text::new("Cells")
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                    .width(Length::Fill)).width(Length::Fill))
+            .into()
+    }
 }
